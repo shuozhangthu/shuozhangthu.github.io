17b9c7a2f40aa53b6c7e3f9f701dacc95e070d14
shuozhangthu <zhangshuo19871127@gmail.com>
2017-03-23 23:02:00 -0500

    dd

---
+++ nikola.py
@@ -0,0 +1,3 @@
+import os
+os.system('nikola build')
+os.system('nikola github_deploy')
--- upload.py
+++
@@ -1,4 +0,0 @@
-import os
-os.system('git add .')
-os.system('git commit -a -m \'add\' ')
-os.system('git push')
