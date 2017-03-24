a9e2e950c19f9b730d9e9112743467b7bbb9bb44
shuozhangthu <zhangshuo19871127@gmail.com>
2017-03-24 15:32:42 -0500

    add

--- nikola.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system('nikola build')
-os.system('nikola github_deploy')
--- upload.py
+++ upload.py
@@ -2,3 +2,5 @@ import os
 os.system('git add .')
 os.system('git commit -a -m \'add\' ')
 os.system('git push')
+os.system('nikola build')
+os.system('nikola github_deploy')
