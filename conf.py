d9619747ef2f69868888024a51619a114f2b472c
shuozhangthu <zhangshuo19871127@gmail.com>
2017-03-24 16:11:35 -0500

    add

--- conf.py
+++ conf.py
@@ -567,7 +567,7 @@ MathJax.Hub.Config({
 # done in the code, hope you don't mind ;-)
 # Note: most Nikola-specific extensions are done via the Nikola plugin system,
 #       with the MarkdownExtension class and should not be added here.
-# MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite']
+MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'tables']
 
 # Social buttons. This is sample code for AddThis (which was the default for a
 # long time). Insert anything you want here, or even make it empty.
