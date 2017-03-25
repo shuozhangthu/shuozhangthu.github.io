af875a36c6b62b3f1151e7cb7a28d730708b876a
shuozhangthu <zhangshuo19871127@gmail.com>
2017-03-25 17:27:43 -0500

    add

--- conf.py
+++ conf.py
@@ -786,7 +786,7 @@ NAVIGATION_LINKS = {
             ('/index.html', 'Home', 'icon-home'),
             ('http://shuozhang.us', 'About me', 'icon-user'),
             ('https://github.com/shuozhangthu', 'My Github', 'icon-github'),
-            ('https://twitter.com/zhangs06', 'My Twitter', 'icon-twitter'),
+            ('https://twitter.com/zhangs06', 'My Twitter', 'icon-linkedin'),
             ('/archive.html', 'Archives', 'icon-folder-open-alt'),
             ('/categories/index.html', 'Tags', 'icon-tags'),
             ('/rss.xml', 'RSS', 'icon-rss'),
